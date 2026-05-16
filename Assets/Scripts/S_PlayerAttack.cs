44d9c1507cee0e01cdf3e7e887317dbead6ffc2b
Meritxell Algueró <merialguerom@gmail.com>
2026-05-16 22:30:37 +0200

    Eliminando todo lo que no se necesita en el script S_PlayerAttack

--- Assets/Scripts/S_PlayerAttack.cs
+++ Assets/Scripts/S_PlayerAttack.cs
@@ -11,17 +11,9 @@ public class S_PlayerAttack : MonoBehaviour
     [Header("Attack Settings")]
     [SerializeField] private float attackDuration = 0.5f;
 
-    [Header("Attack Positions")]
-    private Vector3 idleAttackOffset = new Vector3(0.1f, 0.13f, 0f);
-    [SerializeField] private Vector3 crouchAttackOffset = new Vector3(0.1f, 0.05f, 0f);
-    [SerializeField] private Vector3 jumpAttackOffset = new Vector3(0.15f, 0.25f, 0f);
-
+   
     private bool isAttacking = false;
     private string playerTag;
-
-    public bool isBended = false;
-    public bool isGrounded = true;
-    public bool facingRight = true;
 
     private void Awake()
     {
@@ -33,33 +25,14 @@ public class S_PlayerAttack : MonoBehaviour
     {
         if (playerTag == "Player1" && Input.GetKeyDown(KeyCode.F) && !isAttacking)
         {
-            SetAttackBoxPosition();
             StartCoroutine(PerformAttack());
         }
         if (playerTag == "Player2" && Input.GetKeyDown(KeyCode.RightShift) && !isAttacking)
         {
-            SetAttackBoxPosition();
             StartCoroutine(PerformAttack());
         }
     }
 
-     private void SetAttackBoxPosition()
-    {
-         Vector3 offset = idleAttackOffset;
-        if (isBended)
-        {
-            offset = crouchAttackOffset; 
-        }
-        else if (!isGrounded)
-        {
-            offset = jumpAttackOffset;
-        }
-
-        offset.x = offset.x * (facingRight ? 1f : -1f);
-        attackBox.transform.localPosition = offset;
-    }
-
-    
      private System.Collections.IEnumerator PerformAttack()
     {
         isAttacking = true;
