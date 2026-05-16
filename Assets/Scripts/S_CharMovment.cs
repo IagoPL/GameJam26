ffd7daf0ccb6c8430960c69ef3ca2d511ee2f5b9
Meritxell Algueró <merialguerom@gmail.com>
2026-05-16 13:04:31 +0200

    Añadiendo animaciones

--- Assets/Scripts/S_CharMovment.cs
+++ Assets/Scripts/S_CharMovment.cs
@@ -3,66 +3,81 @@ using UnityEngine;
 public class S_CharMovment : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
     public float speed = 5f;
     public float jumpForce = 7f;
 
-    private float move = 0f;
     private bool isGrounded = false;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
 
     private void Awake()
     {
-        rb = GetComponent <Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+        anim = GetComponent<Animator>();
     }
 
-    public void Update()
+    private void Update()
     {
-        if (rb.GetComponent<Rigidbody2D>() == null)
-            return;
-        
-        MovementCharacter();
-        
+        HandleMovement();
     }
-    private void MovementCharacter ()
+
+    private void HandleMovement()
     {
         float move = 0f;
-        if (CompareTag("Player1"))
+        if (CompareTag("Player1")) //WASD
         {
             if (Input.GetKey(KeyCode.A)) move = -1f;
-            if (Input.GetKey(KeyCode.D)) move = 1f;
+            else if (Input.GetKey(KeyCode.D)) move = 1f;
 
             if (Input.GetKeyDown(KeyCode.W) && isGrounded)
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             }
         }
-        if (CompareTag("Player2"))
+        if (CompareTag("Player2")) //Arrows
         {
             if (Input.GetKey(KeyCode.LeftArrow)) move = -1f;
-            if (Input.GetKey(KeyCode.RightArrow)) move = 1f;
+            else if (Input.GetKey(KeyCode.RightArrow)) move = 1f;
 
             if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             }
         }
-
         rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+
+        UpdateAnimations(move);
+    }
+
+    private void UpdateAnimations(float move)
+    {
+        if (Mathf.Abs(move) > 0.1f && isGrounded)
+            anim.SetBool("ifWalking", true);
+        else
+            anim.SetBool("ifWalking", false);
+
+        if (!isGrounded)
+            anim.SetBool("ifJumping", true);
+        else
+            anim.SetBool("ifJumping", false);
+
+        if (anim.GetBool("ifAttacking"))
+            anim.SetBool("ifAttacking", false);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-        {
             isGrounded = true;
-        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-        {
             isGrounded = false;
-        }
     }
 }
